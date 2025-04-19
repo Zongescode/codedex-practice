59d15f2916483494a6db5965a8696841e842032b
Zongescode <zapotichn125@gmail.com>
2025-04-19 17:09:33 +0100

    good_morning

---
+++ Bonus_Fruit_Finder_debug.js
@@ -0,0 +1,16 @@
+/*  OUTGOING CODE
+let apples = 5;
+let oranges = 15;
+let bananas = 3;
+
+console.log("I have " + apples + " apples 🍎);
+console.log("I have" +  oranges + "oranges 🍊" */
+
+//debugging
+let apples = 5;
+let oranges = 15;
+let bananas = 3;
+
+console.log("I have " + apples + " apples 🍎");
+console.log("  I have", oranges ,"oranges 🍊");
+// was mising 1 quotes and 1 parenthesis 
---
+++ good_morning.js
@@ -0,0 +1,14 @@
+// №12 Good Morning from Codedex 
+// Create a good-morning.js program that prints a message if it's early in the day.
+
+let hour = 2; // curent time of day
+
+if (hour < 12) {
+    console.log("Good Morning 🌞");
+    console.log("Morining routin begin with");
+    console.log("Streach,ect");
+    hour = 3;
+} 
+if (hour = 3) {
+    console.log('HA-HA')
+}
